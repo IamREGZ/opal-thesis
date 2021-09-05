f79b843833c248784e5f3253bccaf4893681c0bf
Ainel08 <ainelpaul995@gmail.com>
2021-09-05 14:21:10 +0700

    update sign-up

---
+++ app/src/main/java/edu/cccdci/opal/sign_up_page.kt
@@ -0,0 +1,11 @@
+package edu.cccdci.opal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sign_up_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_page)
+    }
+}
---
+++ app/src/main/java/edu/cccdci/opal/signing_up.kt
@@ -0,0 +1,13 @@
+package edu.cccdci.opal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signing_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signing_up)
+
+
+    }
+}
