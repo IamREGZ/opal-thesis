6367fbb8b8635eb936ecf75588a060d6cc602ded
Ainel08 <ainelpaul995@gmail.com>
2021-10-04 04:36:10 +0800

    Done Task

---
+++ app/src/main/java/edu/cccdci/opal/activities/CartActivity.kt
@@ -0,0 +1,12 @@
+package edu.cccdci.opal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.cccdci.opal.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
